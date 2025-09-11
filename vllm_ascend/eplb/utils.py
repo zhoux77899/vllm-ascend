f5f88f78b93b0f9df4ff32fb932dd6de8aa36308
shiyuan680 <72335504+shiyuan680@users.noreply.github.com>
2025-09-11 16:12:09 +0800

    [v0.9.1]fix bug of eplb (#2839)
    ### What this PR does / why we need it?
    fix bug of eplb about deepseek v2 model
    
    Signed-off-by: shiyuan680 <917935075@qq.com>

--- vllm_ascend/eplb/utils.py
+++ vllm_ascend/eplb/utils.py
@@ -30,7 +30,8 @@ def get_log2phy_map(self, layer_id):
 def get_all_expert_map(self, num_moe_layers):
     all_loads = []
     for layer_id in range(num_moe_layers):
-        load_tensor = self.get_expert_map(layer_id)  # (num_experts_per_layer,)
+        load_tensor = self.get_expert_map(
+            layer_id + self.num_dense_layers)  # (num_experts_per_layer,)
         all_loads.append(load_tensor)
 
     return torch.stack(all_loads, dim=0)
@@ -38,7 +39,8 @@ def get_all_expert_map(self, num_moe_layers):
 
 def get_all_moe_loads(self):
     all_moe_loads = torch.stack(
-        [self.model.layers[layer_id].mlp.experts.moe_load \
+        [self.model.layers[layer_id +
+                           self.num_dense_layers].mlp.experts.moe_load \
          for layer_id in range(self.num_moe_layers)],
         dim=0
     )
@@ -47,7 +49,8 @@ def get_all_moe_loads(self):
 
 def clear_all_moe_loads(self):
     for layer_id in range(self.num_moe_layers):
-        self.model.layers[layer_id].mlp.experts.clear_moe_load()
+        self.model.layers[layer_id +
+                          self.num_dense_layers].mlp.experts.clear_moe_load()
 
 
 def model_register(model, model_config):
@@ -58,11 +61,12 @@ def model_register(model, model_config):
     model.clear_all_moe_loads = types.MethodType(clear_all_moe_loads, model)
 
     config = model_config.hf_config
+    model.num_dense_layers = 0
 
     if config.model_type == "qwen3_moe":
         model.num_moe_layers = config.num_hidden_layers
     elif config.model_type == "deepseek_v2":
-        num_dense_layers = config.first_k_dense_replace
-        model.num_moe_layers = config.num_hidden_layers - num_dense_layers
+        model.num_dense_layers = config.first_k_dense_replace
+        model.num_moe_layers = config.num_hidden_layers - model.num_dense_layers
     else:
         raise NotImplementedError("EPLB is not supported.")
