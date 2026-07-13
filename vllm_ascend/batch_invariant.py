63183aa9525b2f5ea6f5053fd300d79d4b7ef630
wangx700 <wangxin700@huawei.com>
2026-07-13 09:49:02 +0800

    [Ops][BugFix] Enabled deterministic computation operators for batch invariance (#11818)
    ### What this PR does / why we need it?
    This PR enables deterministic computation algorithms in PyTorch by
    calling `torch.use_deterministic_algorithms(True, warn_only=True)`
    within `override_envs_for_invariance()`. This ensures batch-invariant
    behavior on Ascend NPUs by forcing deterministic operator execution
    where possible.
    
    - vLLM version: v0.23.0
    - vLLM main:
    https://github.com/vllm-project/vllm/commit/1f486d96a17303ce8db8e02be39545b2be338446
    ---------
    Signed-off-by: wangx700 <wangxin700@huawei.com>

--- vllm_ascend/batch_invariant.py
+++ vllm_ascend/batch_invariant.py
@@ -82,9 +82,14 @@ def override_envs_for_invariance():
 
     os.environ["HCCL_DETERMINISTIC"] = "strict"
     os.environ["LCCL_DETERMINISTIC"] = "1"
+
+    # Enable deterministic computation for operators. Some operators on Ascend A5
+    # do not have deterministic mode enabled by default and must be explicitly set.
+    torch.use_deterministic_algorithms(True, warn_only=True)
+
     logger.debug(
         "Batch-invariant env override: weight_nz_mode=0, enable_matmul_allreduce=False, "
-        "HCCL_DETERMINISTIC=strict, LCCL_DETERMINISTIC=1",
+        "HCCL_DETERMINISTIC=strict, LCCL_DETERMINISTIC=1, use_deterministic_algorithms=True",
     )
 
 
